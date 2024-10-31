618c838d20b87cf923ffb90bb3709b50369346d6
Elliott-Roach <elliott.roach@mths.ca>
2024-10-31 10:51:12 -0400

    code

--- main.ts
+++ main.ts
@@ -2,7 +2,86 @@
  *
  * Created by: Elliott
  * Created on: oct 2024
- * This program ...
+ * This program moves a sprite in a clockwise and conterclockwise direction
 */
 
+let sprite: game.LedSprite = null
+let loopCounter = 0
 
+//clean
+basic.clearScreen()
+
+
+input.onButtonPressed(Button.A, function () {
+    // setup
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter = loopCounter + 1
+        basic.pause(500)
+    }
+
+    loopCounter = 0
+    while (loopCounter <= 5 ) {
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter + 1
+        basic.pause(500)
+    }
+
+    loopCounter = 5
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter = loopCounter - 1
+        basic.pause(500)
+    }
+
+    loopCounter = 5
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter - 1
+        basic.pause(500)
+    }
+})
+
+input.onButtonPressed(Button.B, function () {
+
+    // setup
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+
+    //down
+    loopCounter = 0
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter + 1
+        basic.pause(500)
+    }
+
+    //right
+    loopCounter = 0
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter = loopCounter + 1
+        basic.pause(500)
+    }
+
+    //up
+    loopCounter = 5
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter - 1
+        basic.pause(500)
+    }
+
+    //left
+    loopCounter = 5
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter = loopCounter - 1
+        basic.pause(500)
+    }
+    sprite.delete()
+})
