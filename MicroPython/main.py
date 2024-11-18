d0add30b2ae2f5cc8daa9b446b0887df18274ffd
Elliott-Roach <elliott.roach@mths.ca>
2024-11-18 16:18:07 +0000

    pithon

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -6,5 +6,10 @@ This module is a Micro:bit MicroPython program
 
 from microbit import *
 
+create_sprite = null
+loopCounter = int(0)
 
-display.scroll("Hello, World!")
+while True:
+    if button_a.is_pressed():
+        def game():create_sprite
+        (x: 4, y: 4): game.LedSprite
