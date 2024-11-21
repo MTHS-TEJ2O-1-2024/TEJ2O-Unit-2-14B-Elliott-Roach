f60a7baf8da1a81d75ea16954230abee707658aa
Elliott-Roach <elliott.roach@mths.ca>
2024-11-21 16:24:02 +0000

    pithon

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -6,10 +6,16 @@ This module is a Micro:bit MicroPython program
 
 from microbit import *
 
-create_sprite = null
+create_sprite = 2
 loopCounter = int(0)
+display.clear()
 
 while True:
     if button_a.is_pressed():
-        def game():create_sprite
-        (x: 4, y: 4): game.LedSprite
+        display.clear()
+        display.set_pixel(create_sprite, 0, 0)
+
+
+    loopCounter = 0
+    while loopCounter <= 5 :
+    display.set_pixel(create_sprite, 0 1)
